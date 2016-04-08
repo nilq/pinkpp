3007beaa003379203b3ddb5808901dd955dfc167
ubsan <npmazzuca@gmail.com>
2016-04-08 12:48:13 -0700

    New style

--- src/driver.rs
+++ src/driver.rs
@@ -4,13 +4,13 @@ extern crate argparse;
 extern crate typed_arena;
 
 macro_rules! fl {
-    () => ((file!(), line!()));
-    (line:$expr) => ((file!(), line));
+  () => ((file!(), line!()));
+  (line:$expr) => ((file!(), line));
 }
 
 enum Either<L, R> {
-    Left(L),
-    Right(R),
+  Left(L),
+  Right(R),
 }
 
 // TODO(ubsan): unify parameter ordering
@@ -23,58 +23,58 @@ use parse::Lexer;
 use ast::Ast;
 
 fn main() {
-    use std::io::Read;
+  use std::io::Read;
 
-    let mut name = "".to_owned();
-    let mut output = None;
-    let mut print_mir = false;
-    let mut print_llir = false;
-    let mut opt = false;
-    {
-        use argparse::{ArgumentParser, Store, StoreOption, StoreTrue};
+  let mut name = "".to_owned();
+  let mut output = None;
+  let mut print_mir = false;
+  let mut print_llir = false;
+  let mut opt = false;
+  {
+    use argparse::{ArgumentParser, Store, StoreOption, StoreTrue};
 
-        let mut ap = ArgumentParser::new();
-        ap.set_description("The pinkc compiler for the pink language.\n\
-            Written in Rust.");
-        ap.refer(&mut name).required().add_argument("name", Store,
-            "The file to compile");
-        ap.refer(&mut output).add_option(&["-o", "--output"], StoreOption,
-            "The file to output to");
-        ap.refer(&mut print_mir).add_option(&["--print-mir"], StoreTrue,
-            "Pass if you would like to print the generated MIR");
-        ap.refer(&mut print_llir).add_option(&["--print-llir"], StoreTrue,
-            "Pass if you would like to print the generated LLVM IR");
-        ap.refer(&mut opt).add_option(&["--opt", "-O"], StoreTrue,
-            "Pass if you would like to optimize the generated LLVM IR");
+    let mut ap = ArgumentParser::new();
+    ap.set_description("The pinkc compiler for the pink language.\n\
+      Written in Rust.");
+    ap.refer(&mut name).required().add_argument("name", Store,
+      "The file to compile");
+    ap.refer(&mut output).add_option(&["-o", "--output"], StoreOption,
+      "The file to output to");
+    ap.refer(&mut print_mir).add_option(&["--print-mir"], StoreTrue,
+      "Pass if you would like to print the generated MIR");
+    ap.refer(&mut print_llir).add_option(&["--print-llir"], StoreTrue,
+      "Pass if you would like to print the generated LLVM IR");
+    ap.refer(&mut opt).add_option(&["--opt", "-O"], StoreTrue,
+      "Pass if you would like to optimize the generated LLVM IR");
 
-        ap.parse_args_or_exit();
-    }
+    ap.parse_args_or_exit();
+  }
 
-    let output = output.unwrap_or(get_output_from_name(&name));
+  let output = output.unwrap_or(get_output_from_name(&name));
 
-    let mut file = Vec::new();
-    std::fs::File::open(&name).expect(&name)
-        .read_to_end(&mut file).unwrap();
-    let file = String::from_utf8(file).unwrap();
-    let lexer = Lexer::new(&file);
-    let tyctxt = ty::TypeContext::new();
+  let mut file = Vec::new();
+  std::fs::File::open(&name).expect(&name)
+    .read_to_end(&mut file).unwrap();
+  let file = String::from_utf8(file).unwrap();
+  let lexer = Lexer::new(&file);
+  let tyctxt = ty::TypeContext::new();
 
-    let ast = match Ast::create(lexer, &tyctxt) {
-        Ok(ast) => ast,
-        Err(e) => panic!("\n{:#?}", e),
-    };
-    let mir = match ast.typeck(opt) {
-        Ok(mir) => mir,
-        Err(e) => panic!("\n{:#?}", e),
-    };
-    if print_mir {
-        println!("{}", mir);
-    }
+  let ast = match Ast::create(lexer, &tyctxt) {
+    Ok(ast) => ast,
+    Err(e) => panic!("\n{:#?}", e),
+  };
+  let mir = match ast.typeck(opt) {
+    Ok(mir) => mir,
+    Err(e) => panic!("\n{:#?}", e),
+  };
+  if print_mir {
+    println!("{}", mir);
+  }
 
-    mir.build_and_write(&output, print_llir)
+  mir.build_and_write(&output, print_llir)
 }
 
 // TODO(ubsan): take off the ".pnk" of the input file
 fn get_output_from_name(name: &str) -> String {
-    format!("{}.s", name)
+  format!("{}.s", name)
 }
--- src/mir/fmt.rs
+++ src/mir/fmt.rs
@@ -1,183 +1,181 @@
 use super::{Mir, Function, Terminator, Statement, Lvalue, Literal,
-    Value, ValueKind, Local};
+  Value, ValueKind, Local, Parameter};
 use std::fmt::{Debug, Display, Formatter, Error};
 
 impl<'t> Display for Function<'t> {
-    fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
-        for var in self.locals.iter() {
-            try!(writeln!(f, "  let {}: {};", var.0, var.1));
+  fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
+    for (i, var) in self.locals.iter().enumerate() {
+      try!(writeln!(f, "  let v{}: {};", i, var));
+    }
+    for (i, tmp) in self.temporaries.iter().enumerate() {
+      try!(writeln!(f, "  let tmp{}: {};", i, tmp));
+    }
+    for (i, block) in self.blocks.iter().enumerate() {
+      try!(writeln!(f, "  bb{}: {{", i));
+      for stmt in &block.statements {
+        try!(writeln!(f, "  {};", stmt));
+      }
+      try!(writeln!(f, "  {};", block.terminator));
+      try!(writeln!(f, "  }}"));
+    }
+    Ok(())
+  }
+}
+
+impl Display for Terminator {
+  fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
+    match *self {
+      Terminator::Goto(ref b) => write!(f, "goto -> bb{}", b.0),
+      Terminator::Return => write!(f, "return"),
+      Terminator::If {
+        ref cond,
+        ref then_blk,
+        ref else_blk,
+      } => write!(f, "if({}) -> [true: bb{}, false: bb{}]", cond,
+        then_blk.0, else_blk.0),
+    }
+  }
+}
+
+impl Display for Statement {
+  fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
+    write!(f, "{} = {}", self.0, self.1)
+  }
+}
+
+impl Display for Lvalue {
+  fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
+    match *self {
+      Lvalue::Return => write!(f, "return"),
+      Lvalue::Local(ref loc) => write!(f, "{}", loc),
+      Lvalue::Deref(ref ptr) => write!(f, "*{}", ptr),
+    }
+  }
+}
+
+impl Display for Literal {
+  fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
+    match *self {
+      Literal::Int {
+        signed,
+        value,
+        ..
+      } => {
+        if signed {
+          write!(f, "{}", value as i64)
+        } else {
+          write!(f, "{}", value as u64)
         }
-        for (i, block) in self.blocks.iter().enumerate() {
-            try!(writeln!(f, "  bb{}: {{", i));
-            for stmt in &block.statements {
-                try!(writeln!(f, "    {};", stmt.display(self)));
-            }
-            try!(writeln!(f, "    {};", block.terminator.display(self)));
-            try!(writeln!(f, "  }}"));
+      }
+      Literal::Bool(ref value) => write!(f, "{}", value),
+      Literal::Tuple(ref v) => {
+        try!(write!(f, "("));
+        if v.len() == 0 {
+          write!(f, ")")
+        } else {
+          for el in &v[..v.len() - 1] {
+            try!(write!(f, "{}, ", el));
+          }
+          write!(f, "{})", v[v.len() - 1])
         }
-        Ok(())
+      }
     }
+  }
 }
 
-// TODO(ubsan): could probably do this without the allocations of String
-impl Statement {
-    fn display(&self, func: &Function) -> String {
-        format!("{} = {}", (self.0).display(func), (self.1).display(func))
-    }
-}
+impl Display for Value {
+  fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
+    match self.0 {
+      ValueKind::Literal(ref lit) => write!(f, "lit {}", lit),
+      ValueKind::Parameter(ref par) => write!(f, "{}", par),
+      ValueKind::Lvalue(ref lv) => write!(f, "{}", lv),
+      ValueKind::Pos(ref inner) => write!(f, "Pos({})", inner),
+      ValueKind::Neg(ref inner) => write!(f, "Neg({})", inner),
+      ValueKind::Not(ref inner) => write!(f, "Not({})", inner),
+      ValueKind::Ref(ref inner) => write!(f, "&{}", inner),
+      ValueKind::Add(ref lhs, ref rhs)
+        => write!(f, "Add({}, {})", lhs, rhs),
+      ValueKind::Sub(ref lhs, ref rhs)
+        => write!(f, "Sub({}, {})", lhs, rhs),
+      ValueKind::Mul(ref lhs, ref rhs)
+        => write!(f, "Mul({}, {})", lhs, rhs),
+      ValueKind::Div(ref lhs, ref rhs)
+        => write!(f, "Div({}, {})", lhs, rhs),
+      ValueKind::Rem(ref lhs, ref rhs)
+        => write!(f, "Rem({}, {})", lhs, rhs),
+      ValueKind::And(ref lhs, ref rhs)
+        => write!(f, "And({}, {})", lhs, rhs),
+      ValueKind::Xor(ref lhs, ref rhs)
+        => write!(f, "And({}, {})", lhs, rhs),
+      ValueKind::Or(ref lhs, ref rhs)
+        => write!(f, "And({}, {})", lhs, rhs),
+      ValueKind::Shl(ref lhs, ref rhs)
+        => write!(f, "Shl({}, {})", lhs, rhs),
+      ValueKind::Shr(ref lhs, ref rhs)
+        => write!(f, "Shr({}, {})", lhs, rhs),
 
-impl Lvalue {
-    fn display(&self, func: &Function) -> String {
-        match *self {
-            Lvalue::Return => "return".to_owned(),
-            Lvalue::Local(ref loc) => loc.display(func),
-            Lvalue::Deref(ref ptr) =>
-                "*".to_owned() + &ptr.display(func)
+      ValueKind::Eq(ref lhs, ref rhs)
+        => write!(f, "Eq({}, {})", lhs, rhs),
+      ValueKind::Neq(ref lhs, ref rhs)
+        => write!(f, "Neq({}, {})", lhs, rhs),
+      ValueKind::Lt(ref lhs, ref rhs)
+        => write!(f, "Lt({}, {})", lhs, rhs),
+      ValueKind::Lte(ref lhs, ref rhs)
+        => write!(f, "Lte({}, {})", lhs, rhs),
+      ValueKind::Gt(ref lhs, ref rhs)
+        => write!(f, "Gt({}, {})", lhs, rhs),
+      ValueKind::Gte(ref lhs, ref rhs)
+        => write!(f, "Gte({}, {})", lhs, rhs),
+
+      ValueKind::Call {
+        ref callee,
+        ref args,
+      } => {
+        try!(write!(f, "{}(", callee));
+        if args.len() != 0 {
+          for arg in &args[..args.len() - 1] {
+            try!(write!(f, "{}, ", arg));
+          }
+          try!(write!(f, "{}", args[args.len() - 1]));
         }
+        write!(f, ")")
+      }
     }
+  }
 }
 
-impl Terminator {
-    fn display(&self, func: &Function) -> String {
-        match *self {
-            Terminator::Goto(ref b) => format!("goto -> bb{}", b.0),
-            Terminator::Return => "return".to_owned(),
-            Terminator::If {
-                ref cond,
-                ref then_blk,
-                ref else_blk,
-            } => format!("if({}) -> [true: bb{}, false: bb{}]",
-                cond.display(func), then_blk.0, else_blk.0),
-        }
-    }
+impl Display for Local {
+  fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
+    write!(f, "v{}", self.0)
+  }
 }
 
-impl Local {
-    fn display(&self, func: &Function) -> String {
-        func.get_local_name(*self).to_owned()
-    }
+impl Display for Parameter {
+  fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
+    write!(f, "par{}", self.0)
+  }
 }
-
-impl Value {
-    fn display(&self, func: &Function) -> String {
-        match self.0 {
-            ValueKind::Literal(ref lit) => format!("lit {}", lit.display(func)),
-            ValueKind::Parameter(ref par) => format!("par{}", par.0),
-            ValueKind::Lvalue(ref lv) => format!("{}", lv.display(func)),
-            ValueKind::Pos(ref inner) => format!("Pos({})",
-                inner.display(func)),
-            ValueKind::Neg(ref inner) => format!("Neg({})",
-                inner.display(func)),
-            ValueKind::Not(ref inner) => format!("Not({})",
-                inner.display(func)),
-            ValueKind::Ref(ref inner) => format!("&{}",
-                inner.display(func)),
-            ValueKind::Add(ref lhs, ref rhs)
-                => format!("Add({}, {})", lhs.display(func), rhs.display(func)),
-            ValueKind::Sub(ref lhs, ref rhs)
-                => format!("Sub({}, {})", lhs.display(func), rhs.display(func)),
-            ValueKind::Mul(ref lhs, ref rhs)
-                => format!("Mul({}, {})", lhs.display(func), rhs.display(func)),
-            ValueKind::Div(ref lhs, ref rhs)
-                => format!("Div({}, {})", lhs.display(func), rhs.display(func)),
-            ValueKind::Rem(ref lhs, ref rhs)
-                => format!("Rem({}, {})", lhs.display(func), rhs.display(func)),
-            ValueKind::And(ref lhs, ref rhs)
-                => format!("And({}, {})", lhs.display(func), rhs.display(func)),
-            ValueKind::Xor(ref lhs, ref rhs)
-                => format!("And({}, {})", lhs.display(func), rhs.display(func)),
-            ValueKind::Or(ref lhs, ref rhs)
-                => format!("And({}, {})", lhs.display(func), rhs.display(func)),
-            ValueKind::Shl(ref lhs, ref rhs)
-                => format!("Shl({}, {})", lhs.display(func), rhs.display(func)),
-            ValueKind::Shr(ref lhs, ref rhs)
-                => format!("Shr({}, {})", lhs.display(func), rhs.display(func)),
-
-            ValueKind::Eq(ref lhs, ref rhs)
-                => format!("Eq({}, {})", lhs.display(func), rhs.display(func)),
-            ValueKind::Neq(ref lhs, ref rhs)
-                => format!("Neq({}, {})", lhs.display(func), rhs.display(func)),
-            ValueKind::Lt(ref lhs, ref rhs)
-                => format!("Lt({}, {})", lhs.display(func), rhs.display(func)),
-            ValueKind::Lte(ref lhs, ref rhs)
-                => format!("Lte({}, {})", lhs.display(func), rhs.display(func)),
-            ValueKind::Gt(ref lhs, ref rhs)
-                => format!("Gt({}, {})", lhs.display(func), rhs.display(func)),
-            ValueKind::Gte(ref lhs, ref rhs)
-                => format!("Gte({}, {})", lhs.display(func), rhs.display(func)),
-
-            ValueKind::Call {
-                ref callee,
-                ref args,
-            } => {
-                let mut s = format!("{}(", callee);
-                if args.len() != 0 {
-                    for arg in &args[..args.len() - 1] {
-                        s.push_str(&arg.display(func));
-                    }
-                    s.push_str(&args[args.len() - 1].display(func));
-                }
-                s.push(')');
-                s
-            }
-        }
-    }
-}
-
-impl Literal {
-    fn display(&self, func: &Function) -> String {
-        match *self {
-            Literal::Int {
-                signed,
-                value,
-                ..
-            } => {
-                if signed {
-                    format!("{}", value as i64)
-                } else {
-                    format!("{}", value as u64)
-                }
-            }
-            Literal::Bool(ref value) => format!("{}", value),
-            Literal::Tuple(ref v) => {
-                if v.len() == 0 {
-                    "()".to_owned()
-                } else {
-                    let mut s = "(".to_owned();
-                    for el in &v[..v.len() - 1] {
-                        s.push_str(&el.display(func));
-                        s.push_str(", ");
-                    }
-                    s.push_str(&v[v.len() - 1].display(func));
-                    s.push(')');
-                    s
-                }
-            }
-        }
-    }
-}
-
 
 impl<'t> Display for Mir<'t> {
-    fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
-        for (name, function) in &self.functions {
-            try!(write!(f, "fn {}(", name));
-            let inputs = function.ty.input();
-            if inputs.len() != 0 {
-                for input in &inputs[..inputs.len() - 1] {
-                    try!(write!(f, "{}, ", input));
-                }
-                try!(write!(f, "{}", inputs[inputs.len() - 1]));
-            }
-            try!(writeln!(f, ") -> {} {{", function.ty.output()));
-            try!(write!(f, "{}", function));
-            try!(writeln!(f, "}}\n"));
+  fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
+    for (name, function) in &self.functions {
+      try!(write!(f, "fn {}(", name));
+      let inputs = function.ty.input();
+      if inputs.len() != 0 {
+        for input in &inputs[..inputs.len() - 1] {
+          try!(write!(f, "{}, ", input));
         }
-        Ok(())
+        try!(write!(f, "{}", inputs[inputs.len() - 1]));
+      }
+      try!(writeln!(f, ") -> {} {{", function.ty.output()));
+      try!(write!(f, "{}", function));
+      try!(writeln!(f, "}}\n"));
     }
+    Ok(())
+  }
 }
 impl<'t> Debug for Mir<'t> {
-    fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
-        write!(f, "{}", self)
-    }
+  fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
+    write!(f, "{}", self)
+  }
 }
